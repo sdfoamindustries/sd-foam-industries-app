da378b972d9776d89b0bc3850fb18240e23b3dcb
S D FOAM INDUSTRIES <deepakkumar0653@gmail.com>
2026-08-22 01:24:31 +0530

    Add Firebase Auth Firestore and Storage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,9 @@ secrets {
 dependencies {
   implementation(platform(libs.androidx.compose.bom))
   implementation(platform(libs.firebase.bom))
+implementation("com.google.firebase:firebase-auth")
+implementation("com.google.firebase:firebase-firestore")
+implementation("com.google.firebase:firebase-storage")
   // implementation(libs.accompanist.permissions)
   implementation(libs.androidx.activity.compose)
   // implementation(libs.androidx.camera.camera2)
